25a3e82556ea662c7cd9727f72aef1823a2a67d3
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-11-25 18:34:49 -0600

    restart project with typescript

--- database.sql
+++
@@ -1,7 +0,0 @@
--- DATABASE NAME "distance_learning_academy"
-
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
