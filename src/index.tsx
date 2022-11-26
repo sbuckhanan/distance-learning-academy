ab49f63d78f5d871bd2105918d2e2482a438b4a3
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-11-25 18:51:18 -0600

    add in server files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
