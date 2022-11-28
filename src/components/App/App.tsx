90e439a8c0ccad18cebb11e8e337a01b483ee4c5
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-11-27 19:37:36 -0600

    redirect to home on login and navbar comp

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import LoginForm from '../LoginPage/LoginPage';
-// import { UserState } from '../../types/types';
+import { UserState } from '../../types/types';
 import { HomePage } from '../HomePage/HomePage';
 import RegisterForm from '../RegisterForm/RegisterForm';
+import NavBar from '../Navbar/Navbar';
 
 function App() {
 	const dispatch = useDispatch();
 
-	// const user = useSelector((store: UserState) => store.user);
+	const user = useSelector((store: UserState) => store.user);
 
 	useEffect(() => {
 		dispatch({ type: 'FETCH_USER' });
@@ -18,8 +19,16 @@ function App() {
 
 	return (
 		<Router>
+			<NavBar />
 			<Route exact path='/'>
-				<LoginForm />
+				{user.id ? (
+					// If the user is already logged in,
+					// redirect to the /user page
+					<Redirect to='/home' />
+				) : (
+					// Otherwise, show the login page
+					<LoginForm />
+				)}
 			</Route>
 			<Route exact path='/home'>
 				<HomePage />
