b5e743a36362307b08a73ad243cc746f2a5aeb62
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-12-04 20:47:04 -0600

    Make class list and popup to add classes

--- src/components/ClassesList/ClassesList.tsx
+++ src/components/ClassesList/ClassesList.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 export function ClassesList() {
+	const [modelOpen, setModelOpen] = useState(false);
+
 	return (
 		<>
 			{/* Remove className [ h-64 ] when adding a card block */}
-			<div className='container mx-auto px-6 mt-10'>
+			<div className='container mx-auto px-auto mt-10'>
 				{/* Remove className [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
 				<div className='w-full h-full rounded'>
-					<button className='bg-blue-500 w-20 h-10 mb-5 rounded'>Add Class</button>
+					<button
+						onClick={() => setModelOpen(true)}
+						className='bg-indigo-600 w-24 h-10 mb-3 rounded hover:bg-indigo-500'>
+						Create Class
+					</button>
 					<div className='overflow-x-auto relative shadow-md sm:rounded-lg'>
 						<table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
 							<thead className='text-xs text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
@@ -81,6 +89,86 @@ export function ClassesList() {
 								</tr>
 							</tbody>
 						</table>
+					</div>
+				</div>
+			</div>
+
+			{/* Create Class Modal */}
+			<div
+				className={
+					modelOpen
+						? 'py-12 mt-10 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0'
+						: 'hidden'
+				}
+				id='modal'>
+				<div role='alert' className='container mx-auto w-11/12 md:w-2/3 max-w-lg'>
+					<div className='relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400'>
+						<h1 className='text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4'>
+							Create New Class
+						</h1>
+						<label
+							htmlFor='name'
+							className='text-gray-800 text-sm font-bold leading-tight tracking-normal'>
+							Class Name
+						</label>
+						<input
+							id='name'
+							className='mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border'
+							placeholder='3rd Period Gym'
+						/>
+						<label
+							htmlFor='subject'
+							className='text-gray-800 text-sm font-bold leading-tight tracking-normal'>
+							Subject
+						</label>
+						<div className='relative mb-5 mt-2'>
+							<input
+								id='subject'
+								className='text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border'
+								placeholder='Math'
+							/>
+						</div>
+						<label
+							htmlFor='numberOfStudents'
+							className='text-gray-800 text-sm font-bold leading-tight tracking-normal'>
+							Maximum # of Students
+						</label>
+						<div className='relative mb-5 mt-2'>
+							<input
+								id='numberOfStudents'
+								className='text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border'
+								placeholder='0000'
+							/>
+						</div>
+						<div className='flex items-center justify-start w-full'>
+							<button className='focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm'>
+								Submit
+							</button>
+							<button className='focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm'>
+								Cancel
+							</button>
+						</div>
+						<button
+							onClick={() => setModelOpen(false)}
+							className='cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600'
+							aria-label='close modal'
+							role='button'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='icon icon-tabler icon-tabler-x'
+								width='20'
+								height='20'
+								viewBox='0 0 24 24'
+								stroke-width='2.5'
+								stroke='currentColor'
+								fill='none'
+								stroke-linecap='round'
+								stroke-linejoin='round'>
+								<path stroke='none' d='M0 0h24v24H0z' />
+								<line x1='18' y1='6' x2='6' y2='18' />
+								<line x1='6' y1='6' x2='18' y2='18' />
+							</svg>
+						</button>
 					</div>
 				</div>
 			</div>
